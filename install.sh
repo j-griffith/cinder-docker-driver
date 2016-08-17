332a328b5bbc7d44a3bb717e21e74c7ee698e6a7
John Griffith <john.griffith8@gmail.com>
2016-08-17 15:11:42 -0600

    Make sure the installers sets the binary perms
    Make sure the installers sets the binary to executable
    after pulling it from the repo.  We should probably just
    make sure we upload the binary with the write perms, but
    for now we'll do this.
    
    Also remove the accidental $ in front of the BIN_NAME
    declaration in install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ set -e
 # or
 #  'wget -qO- https://https://raw.githubusercontent.com/j-griffith/cinder-docker-driver/master/install.sh | sh'
 
-$BIN_NAME=cinder-docker-driver
+BIN_NAME=cinder-docker-driver
 DRIVER_URL="https://github.com/j-griffith/cinder-docker-driver/releases/download/v0.8/cinder-docker-driver"
 BIN_DIR="/usr/bin"
 
@@ -15,6 +15,7 @@ do_install() {
 sudo mkdir -p /var/lib/cinder/dockerdriver
 sudo mkdir -p /var/lib/cinder/mount
 sudo curl -sSL -o $BIN_DIR/$BIN_NAME $DRIVER_URL
+sudo chmod +x $BIN_DIR/$BIN_NAME
 }
 
 do_install
