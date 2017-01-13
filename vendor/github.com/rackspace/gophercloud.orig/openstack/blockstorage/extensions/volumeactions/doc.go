75ef978b8ee3e390f7c138380156f5013e1012e7
Ed Balduf <ed.balduf@solidfire.com>
2017-01-13 10:50:21 -0700

    updated dependencies again.

--- vendor/github.com/rackspace/gophercloud.orig/openstack/blockstorage/extensions/volumeactions/doc.go
+++
@@ -1,5 +0,0 @@
-// Package volumeactions provides special actions on volumes as part of the
-// OpenStack Block Storage service.  Actions are the category of operations
-// that includes things like attach/detach of volumes to a host/instance.
-
-package volumeactions
